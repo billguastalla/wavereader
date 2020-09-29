d7d8c0328d15e381ece9ce05644fd86305545335
Bill Guastalla <billguastalla@gmail.com>
2020-09-29 21:09:57 +0100

    -> Started to build some appropriate tests.

--- test/test.cpp
+++ test/test.cpp
@@ -1,19 +1,41 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 #include <waveread.h>
+#include <array>
 
-int n()
+constexpr char assetPath[8] = "assets/";
+constexpr std::array<char[255], 5> supportedFiles = {
+	"8-bit_unsigned_Sine_Stereo.wav",
+	"8-bit_Sine_Stereo.wav",
+	"16-bit_signed_Sine_Stereo.wav",
+	"24-bit_signed_Sine_Stereo.wav",
+	"32-bit_signed_Sine_Stereo.wav",
+};
+constexpr std::array<char[255], 6> unsupportedFiles = {
+	"32-bit_float_Sine_Stereo.wav",
+	"64-bit_float_Sine_Stereo.wav",
+	"A-Law_Sine_Stereo.wav",
+	"IMA_ADPCM_Sine_Stereo.wav",
+	"MS_ADPCM_Sine_Stereo.wav",
+	"U-Law_Sine_Stereo.wav",
+};
+
+
+TEST_CASE("Check that unsupported files are stated as unsupported.", "[asdf]" )
 {
-	return 1;
+	for (auto unsupported : unsupportedFiles)
+	{
+		std::string name{ assetPath + std::string{unsupported} };
+		std::shared_ptr<std::ifstream> fileStream{ new std::ifstream{name} };
+		AudioReader waveReader{ fileStream,2048u,0.5};
+		REQUIRE(waveReader.open());
+	}
 }
 
-TEST_CASE("nothing useful yet", "[asdf]" )
+TEST_CASE("Check that supported files are supported.", "[asdf]")
 {
-	REQUIRE( n() == 1 );
-	REQUIRE( n() + 1 == 2);
+	for (auto supported : supportedFiles)
+	{
 
-	std::shared_ptr<std::ifstream> asdf{ new std::ifstream{"samples.zip"} };
-
-	AudioReader waveReader{ asdf };
+	}
 }
-
